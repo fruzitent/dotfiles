98c278fd7480c757881e53dbedcbd3359412800f
fruzitent <fruzit@gmail.com>
2023-08-07 00:11:56 +0300

    Bind launch scripts

---
+++ scripts/fan.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+device=$(echo /sys/devices/platform/asus-nb-wmi/hwmon/hwmon?/pwm1_enable)
+
+if [[ ! $(cat "${device}") -eq 0 ]]; then
+  sudo bash -c "echo 0 > ${device}"
+else
+  sudo bash -c "echo 2 > ${device}"
+fi
