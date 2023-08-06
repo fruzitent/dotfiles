bf947323266ef78d90e01203c7858949f3dc1f69
fruzitent <fruzit@gmail.com>
2023-08-07 00:14:20 +0300

    Update autostart

---
+++ scripts/swww.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+directory=${1}
+swww query || swww init
+
+while true; do
+  next_file=$(find "${directory}" -type f | shuf --head-count 1)
+  swww img "${next_file}" --transition-fps 60 --transition-step 2 --transition-type any
+  sleep $((60 * 30))
+done
