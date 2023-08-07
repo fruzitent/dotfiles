5201aa81d4e0ab4459aed260229fecc9261d5708
fruzitent <fruzit@gmail.com>
2023-08-07 20:43:54 +0300

    Allow changing fan speed without password

--- scripts/asus_wmi_fan.sh
+++ scripts/asus_wmi_fan.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 device=$(echo /sys/devices/platform/asus-nb-wmi/hwmon/hwmon?/pwm1_enable)
 
 if [[ ! $(cat "${device}") -eq 0 ]]; then
-  sudo bash -c "echo 0 > ${device}"
+  sudo bash -c "echo '0' > ${device}"
 else
-  sudo bash -c "echo 2 > ${device}"
+  sudo bash -c "echo '2' > ${device}"
 fi
