71ce7959a500cf97360f050367ce2a6a2854d4cc
fruzitent <fruzit@gmail.com>
2023-08-23 04:05:01 +0300

    Styling

--- openwrt/tplink_archer-c6-v2/files/etc/uci-defaults/50-radio.sh
+++ openwrt/tplink_archer-c6-v2/files/etc/uci-defaults/50-radio.sh
@@ -2,17 +2,15 @@
 
 set -ex
 
-RADIO0_KEY=
-RADIO0_SSID=tplink_archer-c6-v2-5g
-
-RADIO1_KEY=
-RADIO1_SSID=tplink_archer-c6-v2
+radio0_key=
+radio0_ssid=tplink_archer-c6-v2-5g
 
 uci set wireless.@wifi-device[0].disabled="0"
 uci set wireless.default_radio0.encryption="sae-mixed"
-uci set wireless.default_radio0.key="${RADIO0_KEY}"
-uci set wireless.default_radio0.ssid="${RADIO0_SSID}"
+uci set wireless.default_radio0.key="${radio0_key}"
+uci set wireless.default_radio0.ssid="${radio0_ssid}"
 uci set wireless.default_radio0.wpa_disable_eapol_key_retries="1"
+
 uci set wireless.radio0.cell_density="0"
 uci set wireless.radio0.channel="157"
 uci set wireless.radio0.country="US"
@@ -20,11 +18,15 @@ uci set wireless.radio0.htmode="VHT40"
 uci set wireless.radio0.noscan="1"
 uci set wireless.radio0.txpower="23"
 
+radio1_key=
+radio1_ssid=tplink_archer-c6-v2
+
 uci set wireless.@wifi-device[1].disabled="0"
 uci set wireless.default_radio1.encryption="psk-mixed"
-uci set wireless.default_radio1.key="${RADIO1_KEY}"
-uci set wireless.default_radio1.ssid="${RADIO1_SSID}"
+uci set wireless.default_radio1.key="${radio1_key}"
+uci set wireless.default_radio1.ssid="${radio1_ssid}"
 uci set wireless.default_radio1.wpa_disable_eapol_key_retries="1"
+
 uci set wireless.radio1.cell_density="0"
 uci set wireless.radio1.channel="9"
 uci set wireless.radio1.country="US"
@@ -33,4 +35,5 @@ uci set wireless.radio1.noscan="1"
 uci set wireless.radio1.txpower="22"
 
 uci commit
+
 wifi reload
--- openwrt/xiaomi_mi-router-3/files/etc/uci-defaults/50-radio.sh
+++ openwrt/xiaomi_mi-router-3/files/etc/uci-defaults/50-radio.sh
@@ -2,17 +2,15 @@
 
 set -ex
 
-RADIO0_KEY=
-RADIO0_SSID=xiaomi_mi-router-3-5g
-
-RADIO1_KEY=
-RADIO1_SSID=xiaomi_mi-router-3
+radio0_key=
+radio0_ssid=xiaomi_mi-router-3-5g
 
 uci set wireless.@wifi-device[0].disabled="0"
 uci set wireless.default_radio0.encryption="sae-mixed"
-uci set wireless.default_radio0.key="${RADIO0_KEY}"
-uci set wireless.default_radio0.ssid="${RADIO0_SSID}"
+uci set wireless.default_radio0.key="${radio0_key}"
+uci set wireless.default_radio0.ssid="${radio0_ssid}"
 uci set wireless.default_radio0.wpa_disable_eapol_key_retries="1"
+
 uci set wireless.radio0.cell_density="0"
 uci set wireless.radio0.channel="157"
 uci set wireless.radio0.country="US"
@@ -20,11 +18,15 @@ uci set wireless.radio0.htmode="VHT40"
 uci set wireless.radio0.noscan="1"
 uci set wireless.radio0.txpower="21"
 
+radio1_key=
+radio1_ssid=xiaomi_mi-router-3
+
 uci set wireless.@wifi-device[1].disabled="0"
 uci set wireless.default_radio1.encryption="psk-mixed"
-uci set wireless.default_radio1.key="${RADIO1_KEY}"
-uci set wireless.default_radio1.ssid="${RADIO1_SSID}"
+uci set wireless.default_radio1.key="${radio1_key}"
+uci set wireless.default_radio1.ssid="${radio1_ssid}"
 uci set wireless.default_radio1.wpa_disable_eapol_key_retries="1"
+
 uci set wireless.radio1.cell_density="0"
 uci set wireless.radio1.channel="9"
 uci set wireless.radio1.country="US"
@@ -33,4 +35,5 @@ uci set wireless.radio1.noscan="1"
 uci set wireless.radio1.txpower="30"
 
 uci commit
+
 wifi reload
