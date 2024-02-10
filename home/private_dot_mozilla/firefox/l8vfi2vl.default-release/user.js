452503198ac5fe95c065d496803a7975d9039d2c
fruzitent <fruzit@gmail.com>
2024-02-10 21:53:22 +0200

    feat: add firefox module

---
+++ home/private_dot_mozilla/firefox/l8vfi2vl.default-release/user.js
@@ -0,0 +1,6 @@
+user_pref("browser.casting.enabled", true);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.toolbars.bookmarks.visibility", "newtab");
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("ui.key.menuAccessKey", 0);
