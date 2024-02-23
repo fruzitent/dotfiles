48a830b0a4d55ac9f032d34535e76c9d6cba32f7
fruzitent <fruzit@gmail.com>
2024-02-24 01:19:04 +0200

    chore(firefox): copy config to windows

---
+++ home/AppData/Roaming/Mozilla/Firefox/Profiles/zkb9gdza.default-release/user.js
@@ -0,0 +1,11 @@
+// @help: https://searchfox.org/mozilla-release/source/browser/app/profile/firefox.js
+// @help: https://searchfox.org/mozilla-release/source/modules/libpref/init/all.js
+// @help: https://searchfox.org/mozilla-release/source/modules/libpref/init/StaticPrefList.yaml
+
+user_pref("browser.casting.enabled", true);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.toolbars.bookmarks.visibility", "newtab");
+user_pref("findbar.highlightAll", true);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("ui.key.menuAccessKey", 0);
