b26ccd3984fac2d7133f96defa56ec52237a9113
fruzitent <fruzit@gmail.com>
2024-02-24 05:09:05 +0200

    feat(firefox): block desktop notifications

--- home/private_dot_mozilla/firefox/l8vfi2vl.default-release/user.js
+++ home/private_dot_mozilla/firefox/l8vfi2vl.default-release/user.js
@@ -7,5 +7,6 @@ user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 user_pref("browser.toolbars.bookmarks.visibility", "newtab");
 user_pref("findbar.highlightAll", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("permissions.default.desktop-notification", 2);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("ui.key.menuAccessKey", 0);
