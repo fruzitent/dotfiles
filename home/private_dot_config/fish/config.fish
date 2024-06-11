aab54413ed1bc7987e2162a129f44d878d10556f
fruzitent <fruzit@gmail.com>
2024-06-11 20:29:12 +0300

    fix(fish): prevent splitting

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 /usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator | while read --local line
-    set --local key (string split --fields 1 "=" $line | string unescape)
-    set --local val (string split --fields 2 "=" $line | string unescape)
+    set --local key (string split --fields 1 --max 1 "=" $line | string unescape)
+    set --local val (string split --fields 2 --max 1 "=" $line | string unescape)
     set --export $key $val
 end
